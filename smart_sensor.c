b82b670ac30151023c98ee3ee8b176287fb85ff9
briato <brunoduarte9798@gmail.com>
2024-03-20 18:47:52 +0000

    Função para mandar valores inteiros feita

--- smart_sensor.c
+++ smart_sensor.c
@@ -15,12 +15,21 @@ uint16_t send_char(unsigned char c) {
 	return SUCCESS;
 }
 
+
 uint16_t send_number(uint16_t number) {
+	
+	if (!number) { //Verificar se o número não é zero
+		send_char('0');
+		return SUCCESS;
+	}
 
-	uint16_t i = 1;
+	uint16_t i = 10000; //Para a div_int o máximo que precisaremos é 10k
 
-	while 
-
+	while (number / i == 0) i /= 10; //Caso 10k seka muito grande, adequamos
+	do {
+		send_char((unsigned char) (number / i) + '0');
+		number %= i;
+	} while(i=i/10) ;  //Fazemos até chegarmos à casa das unidades
 
 	return SUCCESS;
 }
